55669d7b843dc8d6fc3215664077023b85b7222c
mwangisamkariuki <mwangisamkariuki@gmail.com>
2022-11-05 17:10:40 +0300

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - is a function that sorts array of ints
+ * @array: This is the list of integers to sort
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+size_t i, j = 0;
+int temp = 0;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+			temp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = temp;
+			print_array(array, size);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# 0x1B. C - Sorting algorithms & Big O
+# General Objectives
+At least four different sorting algorithms
+What is the Big O notation, and how to evaluate the time complexity of an algorithm
+How to select the best sorting algorithm for a given input
+What is a stable sorting algorithm
+
+# Data Structure and Functions
+
+
+
+# Task 0: Bubble sort.
+Write a function that sorts an array of integers in
+ascending order using the Bubble sort algorithm
